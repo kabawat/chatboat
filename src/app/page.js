4c61d90e45983c9b6f4aa2d3bc4b47ed3f5d6dee
kabawat <118162167+kabawat@users.noreply.github.com>
2024-03-20 16:31:18 +0530

    project set-up

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <Button>hello</Button>
+    </div>
+  )
+}
+
+export default Main
